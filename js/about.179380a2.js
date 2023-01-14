ae380d975a63d573ff0258b5b1928e1772e82c09
ampu <ampu@yandex.ru>
2023-01-14 12:29:55 +0000

    Deploying to gh-pages from @ ampu/otus-vue-lab1@cc7b77d6575f163ae0c76f70bbcdccff5ca15995 🚀

---
+++ js/about.179380a2.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunkotus_vue_lab1"]=self["webpackChunkotus_vue_lab1"]||[]).push([[443],{335:function(u,n,t){t.r(n),t.d(n,{default:function(){return b}});var a=t(821);const e={class:"about"},s=(0,a._)("h1",null,"This is an about page",-1),r=[s];function c(u,n){return(0,a.wg)(),(0,a.iD)("div",e,r)}var o=t(744);const i={},l=(0,o.Z)(i,[["render",c]]);var b=l}}]);
